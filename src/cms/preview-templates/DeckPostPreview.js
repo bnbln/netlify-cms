456ac2ab0435cece4940a43e0521cf619434bfb3
bnbln <30811632+bnbln@users.noreply.github.com>
2019-08-10 00:20:02 +0200

    config and preview

--- src/cms/preview-templates/DeckPostPreview.js
+++ src/cms/preview-templates/DeckPostPreview.js
@@ -6,16 +6,20 @@ const DeckPostPreview = ({ entry, widgetFor }) => (
   <div style={{
     backgroundColor: "black"
   }}>
+    {console.log(widgetFor('body'))}
   <DeckPageTemplate
     content={widgetFor('body')}
     description={entry.getIn(['data', 'description'])}
     image={entry.getIn(['data', 'image'])}
     arkana={entry.getIn(['data', 'arkana'])}
     title={entry.getIn(['data', 'title'])}
-    id={entry.getIn(['data', 'id'])}
-    
+      id={entry.getIn(['data', 'id'])}
+      upsidedown={entry.getIn(['data', 'upsidedown'])}
+      natural={entry.getIn(['data', 'natural'])}
+      color={entry.getIn(['data', 'color'])}
+
    
-    >{console.log(entry.getIn(['data', 'id']))}</DeckPageTemplate>
+    >{console.log(entry.getIn(['data', 'natural']))}</DeckPageTemplate>
 
   </div>
 )
