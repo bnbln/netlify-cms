0b442dae4bd5aaa485d753fc6e8fd32364981e76
bnbln <30811632+bnbln@users.noreply.github.com>
2019-08-11 22:36:43 +0200

    deck page

--- src/pages/deck/index.js
+++ src/pages/deck/index.js
@@ -3,6 +3,9 @@ import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
 import { Link, graphql } from 'gatsby'
 import Layout from '../../components/Layout'
+import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
+import Typography from '@material-ui/core/Typography';
 
 const TagsPage = ({
   data: {
@@ -13,28 +16,62 @@ const TagsPage = ({
   },
 }) => (
     <Layout>
-      <section className="section">
-        <Helmet title={`Tags | ${title}`} />
-        <div className="container content">
-          <div className="columns">
-            <div
-              className="column is-10 is-offset-1"
-              style={{ marginBottom: '6rem' }}
-            >
-              <h1 className="title is-size-2 is-bold-light">Deck</h1>
-              <ul className="taglist">
-                {group.map(tag => (
-                  <li key={tag.fieldValue}>
-                    <Link to={`/arkana/${kebabCase(tag.fieldValue)}/`}>
+      <Helmet title={`Deck | ${title}`} />
+      <Grid container
+        direction="row"
+        justify="center"
+        alignItems="center" style={{
+          paddingRight: 0,
+          marginTop: 50
+        }}>
+        <Grid item xs={10}>
+          <Grid container
+            direction="row"
+            justify="center"
+            alignItems="center" style={{
+              height: "100vh",
+              boxShadow: "-63px 30px 113px 70px black",
+              zIndex: 100,
+              color: "white",
+              position: "relative",
+            }}>
+            <Typography variant="h1" gutterBottom align="center">
+              Deck
+            </Typography>
+            <div>
+              {group.map(tag => (
+                <div key={tag.fieldValue} style={{
+                  marginBottom: 50
+                }}>
+                  {console.log(tag.nodes)}
+                  <Link to={`/arkana/${kebabCase(tag.fieldValue)}/`} style={{ display: "block" }}>
+                    <Typography variant="h5" gutterBottom align="center">
                       {tag.fieldValue} ({tag.totalCount})
+                    </Typography>
                   </Link>
-                  </li>
-                ))}
-              </ul>
+                  <Grid container justify="center"
+                    alignItems="center" spacing={2}>
+                    {tag.nodes.map(card => (
+                      <Grid item xs={6} sm={4} md={3} lg={2} xl={1}>
+                        <Link to={card.fields.slug}>
+                          <img
+                            style={{
+                              width: "100%",
+                              marginBottom: -8
+                            }}
+                            src={!!card.frontmatter.image.childImageSharp ? card.frontmatter.image.childImageSharp.fluid.src : card.frontmatter.image}
+                          ></img>
+                        </Link>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </div>
+              ))}
             </div>
-          </div>
-        </div>
-      </section>
+
+          </Grid>
+        </Grid>
+        </Grid>
     </Layout>
   )
 
@@ -47,13 +84,24 @@ export const tagPageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "deck-page"}}}) {
+    allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "deck-page"}}}, sort: {fields: frontmatter___id}) {
     group(field: frontmatter___arkana) {
       fieldValue
       totalCount
       nodes {
         frontmatter {
           templateKey
+          title
+          image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        }
+        fields {
+          slug
         }
       }
     }
