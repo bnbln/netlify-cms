88a51ec6611f6896116a6493fb86a273cafd148a
bnbln <30811632+bnbln@users.noreply.github.com>
2019-08-12 20:55:50 +0200

    og-image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -64,7 +64,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content={`${withPrefix("/")}img/og-image.jpg`} />
+        <meta property="og:image" content={`${withPrefix("/")}img/download.jpeg`} />
       </Helmet>
       <Grid container
         direction="row"
