59f24c1f845271086782b33062a700b3a0f441d4
bnbln <30811632+bnbln@users.noreply.github.com>
2019-08-20 09:52:51 +0200

    youtube widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -8,8 +8,9 @@ import DeckPostPreview from './preview-templates/DeckPostPreview'
 
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import React from 'react';
 
-// import styles from "../components/all.sass"
+import YouTube from 'react-youtube';
 
 
 CMS.registerMediaLibrary(uploadcare);
@@ -21,3 +22,45 @@ CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('deck', DeckPostPreview)
+
+CMS.registerEditorComponent({
+  // Internal id of the component
+  id: "youtube",
+  // Visible label
+  label: "YouTube",
+  // Fields the user need to fill out when adding an instance of the component
+  fields: [{ name: 'id', label: 'YouTube Video ID', widget: 'string' }],
+  // Pattern to identify a block as being an instance of this component
+  pattern: /^youtube (\S+)$/,
+  // Function to extract data elements from the regexp match
+  fromBlock: function (match) {
+    return {
+      id: match[1]
+    };
+  },
+  // Function to create a text block from an instance of this component
+  toBlock: function (obj) {    
+    return 'youtube ' + obj.id;
+  },
+  // Preview output for this component. Can either be a string or a React component
+  // (component gives better render performance)
+  toPreview: function (obj) {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+    console.log(<YouTube
+        videoId={obj.id}
+        opts={opts}
+        // onReady={this._onReady}
+      />);
+    
+    return (
+      '<img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video"/>'
+
+    );
+  }
+});
